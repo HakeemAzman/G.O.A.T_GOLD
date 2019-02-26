c6de1d17a72952f90f625470d7f106b588ead77e
Nick Lee <44768335+SmallRunt@users.noreply.github.com>
2019-02-26 16:37:12 +0800

    Minor tweaks.
    Fixed a few things for the transitioning of scenes.

---
+++ G.O.A.T_GOLD/Assets/startTransition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startTransition : MonoBehaviour {
+
+    public GameObject transitionStart;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(disableAfter());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator disableAfter()
+    {
+        yield return new WaitForSeconds(1f);
+        transitionStart.gameObject.SetActive(false);
+    }
+}
