fc64a8463bb78caeeb78deddabf45f0d113bb4c3
Hakeem Azman <nurhakeemazman@gmail.com>
2019-02-28 00:53:27 +0800

    Splash Animation Done!

--- G.O.A.T_GOLD/Assets/G.O.A.T/Prototypes/Scripts/ETC/SplashScreen.cs
+++ G.O.A.T_GOLD/Assets/G.O.A.T/Prototypes/Scripts/ETC/SplashScreen.cs
@@ -3,34 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SplashScreen : MonoBehaviour {
+public class SplashScreen : MonoBehaviour
+{
+    public Animator anim;
+    public Animator transitionToBlack;
+    public Animator arcticZen;
+    public Animator click;
 
-    Animator anim;
-    bool hasClicked = false;
+    public AudioSource audio;
 
+    public int i = 0;
+    
 	// Use this for initialization
 	void Start ()
     {
-        anim = GetComponent<Animator>();
+        click = click.GetComponent<Animator>();
+        arcticZen = arcticZen.GetComponent<Animator>();
+        transitionToBlack = transitionToBlack.GetComponent<Animator>();
+        anim = anim.GetComponent<Animator>();
+
+        audio = audio.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	    if(Input.GetMouseButtonDown(0) && !hasClicked)
+	    if(Input.GetMouseButtonDown(0))
         {
+            i++;
+
+            StartCoroutine(Proceed());
+            audio.Play();
             anim.Play("CameraMove");
-            hasClicked = true;
+            click.Play("Fade");
+            arcticZen.Play("ArcticZen");
+            transitionToBlack.Play("TransitionToBlack");
         }
 
-        if(Input.GetMouseButtonDown(0) && hasClicked == true)
+        if(Input.GetMouseButtonDown(0) && i == 2)
         {
             StartCoroutine(Skip());
         }
 	}
 
+    IEnumerator Proceed()
+    {
+        yield return new WaitForSeconds(45f);
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
     IEnumerator Skip()
     {
+        transitionToBlack.Play("SkipCutscene");
+
         yield return new WaitForSeconds(2f);
 
         SceneManager.LoadScene("MainMenu");
